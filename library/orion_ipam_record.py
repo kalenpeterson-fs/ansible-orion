4ed11cc81363fff60095207452f09cb6648869e0
Kalen Peterson <kpeterson@forsythe.com>
2019-09-24 16:39:32 -0700

    updated to include ping test, and SkipScan

--- library/orion_ipam_record.py
+++ library/orion_ipam_record.py
@@ -4,7 +4,7 @@
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 ANSIBLE_METADATA = {
-    'metadata_version': '0.1',
+    'metadata_version': '0.2',
     'status': ['preview'],
     'supported_by': 'community'
 }
@@ -37,6 +37,27 @@ options:
         description:
             - The Orion Server's password
         required: true
+    new_ip_status:
+        description:
+            - The IPAM Record Status to set for the provisioned IP
+            - "Used", "Available", "Reserved", "Transient", "Blocked"
+        required: false
+        default: "Used"
+    new_ip_comment:
+        description:
+            - The IPAM Record Comment to set for the provisioned IP
+        required: false
+        default: "Updated by Ansible"
+    ping_test:
+        description:
+            - Option to perform a ping test before returning the IP
+        required: false
+        default: true
+    retry_limit:
+        description:
+            - Number of times to validate an IP from Orion before failing
+        required: false
+        default: 5
 
 author:
     - Kalen Peterson (@kpeterson-sirius)
@@ -45,12 +66,23 @@ author:
 EXAMPLES = '''
 # Get the next available IP Address in 10.0.0.0/24
 - name: Get Next IP
-  orion_imap_record:
-    subnet: '10.0.0.0/24'
+  orion_ipam_record:
+    subnet: '10.0.0.0 /24'
     orion_server: 'orion.server.com'
     orion_username: 'orionapiuser'
     orion_password: 'orionapipass'
 
+# Get the next available IP Address in 10.0.0.0/24, with optional args
+- name: Get Next IP
+  orion_ipam_record:
+    subnet: '10.0.0.0 /24'
+    orion_server: 'orion.server.com'
+    orion_username: 'orionapiuser'
+    orion_password: 'orionapipass'
+    new_ip_status: 'Transient'
+    new_ip_comment: 'Changed by Script'
+    ping_test: false
+    retry_limit: 1
 '''
 
 RETURN = '''
@@ -61,11 +93,7 @@ ip_address:
 '''
 
 from ansible.module_utils.basic import AnsibleModule
-
-import re
-import requests
-import sys
-import json
+import os
 
 try:
     from orionsdk import SwisClient
@@ -81,11 +109,15 @@ def run_module():
         orion_server=dict(type='str', required=True),
         orion_username=dict(type='str', required=True),
         orion_password=dict(type='str', required=True, no_log=True),
+        new_ip_status=dict(type='str', required=False, default='Used'),
+        new_ip_comment=dict(type='str', required=False, default='Updated by Ansible'),
+        ping_test=dict(type='bool', required=False, default=True),
+        retry_limit=dict(type='int', required=False, default=5)
     )
 
     # Check for ORION
     if not HAS_ORION:
-        module.fail_json(msg="orionsdk is required for this module. pleae install it")
+        module.fail_json(msg="orionsdk is required for this module. please install it")
 
     # seed the result dict in the object
     # we primarily care about changed and state
@@ -111,46 +143,63 @@ def run_module():
     if module.check_mode:
         module.exit_json(**result)
 
-    # manipulate or modify the state as needed (this is going to be the
-    # part where your module will do what it needs to do)
-
-    # Send Request
+    # Send Request for top {retry_limit} IP Addresses
     swis = SwisClient(module.params['orion_server'], module.params['orion_username'], module.params['orion_password'])
     try:
-        results = swis.query("SELECT TOP 1 I.Status, I.IPAddress, I.DisplayName, Uri, I.Comments FROM IPAM.IPNode I WHERE Status=2 AND I.Subnet.DisplayName = @subnet", subnet=module.params['subnet'])
+        results = swis.query("SELECT TOP {retry_limit} I.Status, I.IPAddress, I.DisplayName, Uri, I.Comments FROM IPAM.IPNode I WHERE Status=2 AND I.Subnet.DisplayName = @subnet", subnet=module.params['subnet'])
     except:
-        module.fail_json(msg="Failed to query Orion. Check orion_server, orion_username, and orion_password {0}".format(str(e)))
+        module.fail_json(msg="Failed to query Orion. Check orion_server, orion_username, and orion_password {0}".format(str(e)), **result)
 
-    #Check Request
-    if results["results"][0]:
-        result['ip_address'] = results["results"][0]['IPAddress']
-        uri = results["results"][0]['Uri']
+
+    # Find a Valid/Free IP from the request
+    for i in range(module.params['retry_default']):
+
+        # Check if there was a valid request
+        if not results["results"][i]:
+            continue
+
+        # Perform a Ping Test
+        if module.params['ping_test']:
+            ping_response = os.system("ping -c 1 " + results["results"][i]['IPAddress'])
+            if ping_response == 0:
+                print("{ip_address} is Alive")
+                continue
+            else:
+                print("{ip_address} is Not Alive")
+                ip_address = results["results"][i]['IPAddress']
+                uri = results["results"][i]['Uri']
+                break
+        else:
+            ip_address = results["results"][i]['IPAddress']
+            uri = results["results"][i]['Uri']
+
+    # Did we get an OK IP Address?
+    if ip_address:
+        # Set the Status
+        try:
+            swis.update(uri, Status=module.params['new_ip_status'])
+        except:
+            module.fail_json(msg="Failed to update status {0}".format(str(e)), **result)
+
+        # Set the Comments
+        try:
+            swis.update(uri, Comments=module.params['new_ip_comments'])
+        except:
+            module.fail_json(msg="Failed to update comment {0}".format(str(e)), **result)
+
+        # Set SkipScan to False
+        try:
+            swis.update(uri, SkipScan=False)
+        except:
+            module.fail_json(msg="Failed to set SkipScan to False {0}".format(str(e)), **result)
+
+        # Set the Module Result
+        result['ip_address'] = ip_address
+        result['changed'] = True
     else:
-        module.fail_json(msg='Failed to find an available IP address')
+        module.fail_json(msg="Failed to find an unused IP Address", **result)
 
-    try:
-        swis.update(uri, Status="Used")
-    except:
-        module.fail_json(msg="Failed to update status {0}".format(str(e)))
-
-    try:
-        swis.update(uri, Comments="Updated by Ansible")
-    except:
-        module.fail_json(msg="Failed to update comment {0}".format(str(e)))      
-
-    # use whatever logic you need to determine whether or not this module
-    # made any modifications to your target
-    #if module.params['new']:
-    #    result['changed'] = True
-
-    # during the execution of the module, if there is an exception or a
-    # conditional state that effectively causes a failure, run
-    # AnsibleModule.fail_json() to pass in the message and the result
-    #if module.params['name'] == 'fail me':
-    #    module.fail_json(msg='You requested this to fail', **result)
-
-    # in the event of a successful module execution, you will want to
-    # simple AnsibleModule.exit_json(), passing the key/value results
+    # Exit Module
     module.exit_json(**result)
 
 def main():
