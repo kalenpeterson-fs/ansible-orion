bac59ac8023a1b1d69ff38baeabba57dd2ce3041
Kalen Peterson <kalen.peterson@siriuscom.com>
2023-03-02 18:07:11 +0000

    Added tests

--- tests/ee-test.sh
+++ tests/ee-test.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Build Image
-docker build -t docker.io/kalenpeterson/awx-ee:orion-latest ../ || exit 1
+docker build -t docker.io/kalenpeterson/awx-ee:orion-latest -f Dockerfile ../ || exit 1
 
 # Run Pod
 docker run -it -v /home/dgx/ansible-orion:/mnt docker.io/kalenpeterson/awx-ee:orion-latest bash
+#    ansible-playbook -i /mnt/tests/inventory /mnt/tests/GetIpAddress.yml
